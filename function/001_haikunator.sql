60b0b482f77d7daf58eaee4149d4a7231d7f7bfc
Benji Visser <benny@noqcks.io>
2022-12-22 13:44:22 -0700

    remove raises

--- function/001_haikunator.sql
+++ function/001_haikunator.sql
@@ -3,8 +3,8 @@ RETURNS TEXT AS $$
 DECLARE
   adjectives TEXT[] := '{aged, ancient, autumn, billowing, bitter, black, blue, bold, broad, broken, calm, cold, cool, crimson, curly, damp, dark, dawn, delicate, divine, dry, empty, falling, fancy, flat, floral, fragrant, frosty, gentle, green, hidden, holy, icy, jolly, late, lingering, little, lively, long, lucky, misty, morning, muddy, mute, nameless, noisy, odd, old, orange, patient, plain, polished, proud, purple, quiet, rapid, raspy, red, restless, rough, round, royal, shiny, shrill, shy, silent, small, snowy, soft, solitary, sparkling, spring, square, steep, still, summer, super, sweet, throbbing, tight, tiny, twilight, wandering, weathered, white, wild, winter, wispy, withered, yellow, young}';
   nouns TEXT[] := '{art, band, bar, base, bird, block, boat, bonus, bread, breeze, brook, bush, butterfly, cake, cell, cherry, cloud, credit, darkness, dawn, dew, disk, dream, dust, feather, field, fire, firefly, flower, fog, forest, frog, frost, glade, glitter, grass, hall, hat, haze, heart, hill, king, lab, lake, leaf, limit, math, meadow, mode, moon, morning, mountain, mouse, mud, night, paper, pine, poetry, pond, queen, rain, recipe, resonance, rice, river, salad, scene, sea, shadow, shape, silence, sky, smoke, snow, snowflake, sound, star, sun, sun, sunset, surf, term, thunder, tooth, tree, truth, union, unit, violet, voice, water, waterfall, wave, wildflower, wind, wood}';
-  adjective TEXT := '';
-  noun TEXT := '';
+  adjective := adjectives[floor(random()*(array_length(adjectives, 1)))];
+  noun := nouns[floor(random()*(array_length(nouns, 1)))];
   token_chars TEXT[] := '{}';
   token TEXT := '';
 BEGIN
@@ -18,13 +18,6 @@ BEGIN
   FOR i IN 1..token_length LOOP
     token := token || token_chars[floor(random()*array_length(token_chars, 1))+1];
   END LOOP;
-
-  adjective := adjectives[floor(random()*(array_length(adjectives, 1)))];
-  noun := nouns[floor(random()*(array_length(nouns, 1)))];
-  raise notice 'Token Chars %', token_chars;
-  raise notice 'Adjective %', adjective;
-  raise notice 'Noun %', noun;
-  raise notice 'Token %', token;
 
   RETURN adjective || '-' || noun || '-' || token;
 END;
