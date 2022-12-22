59a16775f39bc94c579897bbd1056c59e78fe0f7
Benji Visser <benny@noqcks.io>
2022-12-22 14:02:46 -0700

    update test

--- tests/haikunator.sql
+++ tests/haikunator.sql
@@ -7,30 +7,28 @@ SELECT ok(haikunate() IS NOT NULL, 'haikunate() returns a non-null value') AS "T
        'haikunate() returns a non-null value' AS "Test Description";
 
 -- Test case 2: Check that the function returns a string with the correct format (adjective-noun-token)
-format (adjective-noun-token)
 SELECT ok(regexp_matches(haikunate(), '^[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z0-9]{4}$') IS NOT NULL,
          'haikunate() returns a string with the correct format (adjective-noun-token)') AS "Test Result",
        'haikunate() returns a string with the correct format (adjective-noun-token)' AS "Test Description";
 
 -- Test case 3: Check that the function returns a string with the correct format (adjective-noun-token) when token_hex is true
-format (adjective-noun-token) when token_hex is true
 SELECT ok(regexp_matches(haikunate(4, '-', TRUE), '^[a-zA-Z]+-[a-zA-Z]+-[a-f0-9]{4}$') IS NOT NULL,
          'haikunate() returns a string with the correct format (adjective-noun-token) when token_hex is true') AS "Test Result",
        'haikunate() returns a string with the correct format (adjective-noun-token) when token_hex is true' AS "Test Description";
 
--- -- Test case 4: Check that the function returns a unique value each time it is called
--- SELECT ok(haikunate() != haikunate(), 'haikunate() returns a unique value each time it is called') AS "Test Result",
---        'haikunate() returns a unique value each time it is called' AS "Test Description";
+-- Test case 4: Check that the function returns a unique value each time it is called
+SELECT ok(haikunate() != haikunate(), 'haikunate() returns a unique value each time it is called') AS "Test Result",
+       'haikunate() returns a unique value each time it is called' AS "Test Description";
 
--- -- Test case 5: Check that the function returns the correct length when token_length is specified
--- SELECT ok(length(regexp_matches(haikunate(8), '^[a-zA-Z]+-[a-zA-Z]+-([a-zA-Z0-9]{8})$')[1]) = 8,
---          'haikunate() returns the correct length when token_length is specified') AS "Test Result",
---        'haikunate() returns the correct length when token_length is specified' AS "Test Description";
+-- Test case 5: Check that the function returns the correct length when token_length is specified
+SELECT ok(regexp_matches(haikunate(8), '^[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z0-9]{8}$') IS NOT NULL,
+         'haikunate() returns a string with the correct length when token_length is specified') AS "Test Result",
+       'haikunate() returns a string with the correct length when token_length is specified' AS "Test Description";
 
--- -- Test case 6: Check that the delimiter is correct when specified
--- SELECT ok(regexp_matches(haikunate(8, '_'), '^[a-zA-Z]+_[a-zA-Z]+_([a-zA-Z0-9]{8})$') IS NOT NULL,
---          'haikunate() returns the correct delimiter when specified') AS "Test Result",
---        'haikunate() returns the correct delimiter when specified' AS "Test Description";
+-- Test case 6: Check that the delimiter is correct when specified
+SELECT ok(regexp_matches(haikunate(4, "_"), '^[a-zA-Z]+_[a-zA-Z]+_[a-zA-Z0-9]{4}$') IS NOT NULL,
+         'haikunate() returns the correct delimineter when a delimiter is specified') AS "Test Result",
+       'haikunate() returns the correct delimineter when a delimiter is specified' AS "Test Description";
 
 -- Finish tests and rollback transaction
 SELECT * FROM finish();
