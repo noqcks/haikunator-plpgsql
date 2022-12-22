3ffca0be71f6e67e86a58172c019d5ab6d8685fe
Benji Visser <benny@noqcks.io>
2022-12-22 13:48:09 -0700

    add test for hex_token true

--- tests/haikunator.sql
+++ tests/haikunator.sql
@@ -11,7 +11,12 @@ SELECT ok(regexp_matches(haikunate(), '^[a-zA-Z]+-[a-zA-Z]+-[a-zA-Z0-9]{8}$') IS
          'haikunate() returns a string with the correct format (adjective-noun-token)') AS "Test Result",
        'haikunate() returns a string with the correct format (adjective-noun-token)' AS "Test Description";
 
--- Test case 3: Check that the function returns a unique value each time it is called
+-- Test case 3: Check that the function returns a string with the correct format (adjective-noun-token) when token_hex is true
+SELECT ok(regexp_matches(haikunate(8, '-', TRUE), '^[a-zA-Z]+-[a-zA-Z]+-[a-f0-9]{8}$') IS NOT NULL,
+         'haikunate() returns a string with the correct format (adjective-noun-token) when token_hex is true') AS "Test Result",
+       'haikunate() returns a string with the correct format (adjective-noun-token) when token_hex is true' AS "Test Description";
+
+-- Test case 4: Check that the function returns a unique value each time it is called
 SELECT ok(haikunate() != haikunate(), 'haikunate() returns a unique value each time it is called') AS "Test Result",
        'haikunate() returns a unique value each time it is called' AS "Test Description";
 
